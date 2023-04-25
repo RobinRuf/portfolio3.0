88bf1055048df61713305cf41b047ebb5458d0d6
Robin <robin@MacbookAir.local>
2023-04-26 00:55:21 +0200

    Bugfixes and small content changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,15 +46,15 @@ const services = [
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: "React Developer",
     icon: mobile,
   },
   {
-    title: "Backend Developer",
+    title: "Full Stack Developer",
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Mobile Developer",
     icon: creator,
   },
 ];
