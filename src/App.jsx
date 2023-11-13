d93a633935490c54b006a364f3296abebf96b3b6
Robin <robin@macbookair.home>
2023-11-13 21:40:52 +0100

    fix(privacy): bugfix

--- src/App.jsx
+++ src/App.jsx
@@ -16,23 +16,25 @@ import PrivacyPolicy from "./components/apps/policies/privacy/chinchin";
 const App = () => {
   return (
     <BrowserRouter>
-        <Route exact path="/">
-          <div className="relative z-0 bg-primary">
-            <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-              <Navbar />
-              <Hero />
-            </div>
-            <About />
-            <Experience />
-            <Tech />
-            <Works />
-            <Feedbacks />
-            <Contact />
-            <StarsCanvas />
-          </div>
-        <Routes>
-          <Route path="/apps/policies/privacy/chinchin" element={<PrivacyPolicy />} />
-        </Routes>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <Contact />
+        <StarsCanvas />
+      </div>
+      <Routes>
+        <Route
+          path="/apps/policies/privacy/chinchin"
+          element={<PrivacyPolicy />}
+        />
+      </Routes>
     </BrowserRouter>
   );
 };
