a6eba98ed9aa0eda9754e22805cbc0ab2ae389e9
Robin <robin@macbookair.home>
2023-11-29 01:22:12 +0100

    chore(work): changed design of the work cards to a glas design with backgroudn nearly transparent
    and backdrop filter with blur

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -27,7 +27,7 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+        className="bg-tertiary/25 backdrop-blur-lg border border-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
       >
         <div className="relative w-full h-[230px]">
           <img
